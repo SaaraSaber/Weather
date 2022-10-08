48f09825ec49698333076a9dc4e9295d5843e5e8
saraSaber <saraaasaber77@gmail.com>
2022-10-08 19:09:10 +0330

    set view & get data

--- app/src/main/java/com/example/weather/view/FiveDayForecastFragment.kt
+++ app/src/main/java/com/example/weather/view/FiveDayForecastFragment.kt
@@ -6,10 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weather.CITY_ID
+import com.example.weather.adapter.FiveDayForecastAdapter
+import com.example.weather.data.DailyForecastsModel
 import com.example.weather.databinding.FragmentFiveDayForecastBinding
+import com.example.weather.viewModel.FiveDayForecastViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FiveDayForecastFragment : Fragment() {
     private lateinit var binding: FragmentFiveDayForecastBinding
+    private val viewModel: FiveDayForecastViewModel by viewModel()
+    private lateinit var adapter: FiveDayForecastAdapter
+    private var cityId: String? = null
+    //private lateinit var lsDailyForecasts: ArrayList<DailyForecastsModel>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,9 +32,25 @@ class FiveDayForecastFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.backBtn.setOnClickListener {
             findNavController().popBackStack()
         }
 
+        cityId = requireArguments().getString(CITY_ID, "0")
+
+        viewModel.sendOnDayForecastApi(requireActivity(), cityId!!)
+        viewModel.liveData.observe(viewLifecycleOwner) {
+            if (it.DailyForecasts.isEmpty()) return@observe
+            adapter = FiveDayForecastAdapter(requireActivity())
+            adapter.lsDailyForecastsModel = it.DailyForecasts
+            setRecFiveDayForecast()
+        }
+    }
+
+    private fun setRecFiveDayForecast() {
+        binding.recFiveDayForecast.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        binding.recFiveDayForecast.adapter = adapter
     }
 }
