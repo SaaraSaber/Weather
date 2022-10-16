4ba34830a927d818b58678c16d57973b91ed8ccb
saraSaber <saraaasaber77@gmail.com>
2022-10-16 19:21:17 +0330

    change apiId

--- app/src/main/java/com/example/weather/common/Unit.kt
+++ app/src/main/java/com/example/weather/common/Unit.kt
@@ -1,5 +1,5 @@
 package com.example.weather
 
-const val API_ID = "ANPgzPV9Y32yPpFXjWsWipKxmkGDNO8f"
+const val API_ID = "wTKTvj4N1D7ULkr69XuGKRjYQoKr1BYh"
 const val CITY_ID = "cityId"
 const val API_RADAR = "2020ed536ff6382cf13d78d23e36f428a14eb244"
